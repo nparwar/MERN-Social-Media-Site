0c59b66ab91c4d46d803cfecc74bdc9145dbe658
nparwar <123691454+nparwar@users.noreply.github.com>
2024-02-10 17:23:05 -0600

    Update pdfEx.py

--- pdfEx.py
+++ pdfEx.py
@@ -9,7 +9,7 @@ import datetime
 import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib.flow import InstalledAppFlow as Flow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import asyncio
