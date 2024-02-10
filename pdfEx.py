74b6f875948e3274ac706a8bdd43840b4d227386
nparwar <123691454+nparwar@users.noreply.github.com>
2024-02-09 21:42:01 -0600

    Update pdfEx.py

--- pdfEx.py
+++ pdfEx.py
@@ -74,7 +74,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-            "../credentials.json", SCOPES
+            "credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=8080)
         # Save the credentials for the next run
